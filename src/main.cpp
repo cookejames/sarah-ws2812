887e28625b17a9372d46082768162c8fef47d21f
James Cooke <james@cookeit.co.uk>
2016-12-17 19:08:56 +0000

    Use ws2812fx library to enable multiple modes

--- src/main.cpp
+++ src/main.cpp
@@ -1,27 +1,21 @@
-#define VERSION "4"
-#include "Adafruit_NeoPixel.h"
+#define VERSION "5"
+#include <WS2812FX.h>
 #include "SarahHome.h"
 #include <ArduinoJson.h>
 
 SarahHome sarahHome("lights");
 
 #define LED_PIN 15
-#define NUMPIXELS 10
+#define NUMPIXELS 100
 #define BUTTON_PIN 0
-Adafruit_NeoPixel pixels = Adafruit_NeoPixel(NUMPIXELS, LED_PIN, NEO_GRB + NEO_KHZ800);
+#define START_SPEED 200
+#define START_BRIGHTNESS 255
+WS2812FX ws2812fx = WS2812FX(NUMPIXELS, LED_PIN, NEO_GRB + NEO_KHZ800);
 volatile int lastTime = 0;
 boolean ledsOn = false;
 int lastRed = 255;
 int lastGreen = 100;
 int lastBlue = 30;
-
-void setStrip(int red, int green, int blue) {
-  for(int i=0;i<NUMPIXELS;i++){
-    pixels.setPixelColor(i, pixels.Color(red,green,blue));
-    pixels.show();
-    delay(100);
-  }
-}
 
 void mqttCallback(char* topic, byte* payload, unsigned int length) {
   char json[length];
@@ -38,24 +32,34 @@ void mqttCallback(char* topic, byte* payload, unsigned int length) {
     return;
   }
 
-  int red = root["r"];
-  int green = root["g"];
-  int blue = root["b"];
+  if (root.containsKey("r") && root.containsKey("g") && root.containsKey("b")) {
+    int red = root["r"];
+    int green = root["g"];
+    int blue = root["b"];
+    ws2812fx.setColor(red, green, blue);
+    if (red == 0 && green == 0 && blue == 0) {
+      ledsOn = false;
+    } else {
+      ledsOn = true;
+      lastRed = red;
+      lastGreen = green;
+      lastBlue = blue;
+    }
+  }
 
-  Serial.print(red);
-  Serial.print(", ");
-  Serial.print(green);
-  Serial.print(", ");
-  Serial.println(blue);
+  if (root.containsKey("mode")) {
+    int mode = root["mode"];
+    ws2812fx.setMode(mode);
+  }
 
-  setStrip(red, green, blue);
-  if (red == 0 && green == 0 && blue == 0) {
-    ledsOn = false;
-  } else {
-    ledsOn = true;
-    lastRed = red;
-    lastGreen = green;
-    lastBlue = blue;
+  if (root.containsKey("speed")) {
+    int speed = root["speed"];
+    ws2812fx.setSpeed(speed);
+  }
+
+  if (root.containsKey("brightness")) {
+    int brightness = root["brightness"];
+    ws2812fx.setBrightness(brightness);
   }
 }
 
@@ -66,10 +70,10 @@ void pinChanged()
 
   if (duration > 500) {
     if (ledsOn) {
-      setStrip(0, 0, 0);
+      ws2812fx.setColor(0, 0, 0);
       ledsOn = false;
     } else {
-      setStrip(lastRed, lastGreen, lastBlue);
+      ws2812fx.setColor(lastRed, lastGreen, lastBlue);
       ledsOn = true;
     }
   }
@@ -84,12 +88,17 @@ void setup() {
   sarahHome.subscribe(subscribeTopic);
   sarahHome.mqttClient.setCallback(mqttCallback);
 
-  pixels.begin(); // This initializes the NeoPixel library.
-  setStrip(lastRed, lastGreen, lastBlue);
+  ws2812fx.init(); // This initializes the NeoPixel library.
+  ws2812fx.setBrightness(START_BRIGHTNESS);
+  ws2812fx.setSpeed(START_SPEED);
+  ws2812fx.setMode(FX_MODE_STATIC);
+  ws2812fx.setColor(lastRed, lastGreen, lastBlue);
+  ws2812fx.start();
   pinMode(BUTTON_PIN, INPUT);
   attachInterrupt(BUTTON_PIN, pinChanged, RISING);
 }
 
 void loop() {
   sarahHome.loop();
+  ws2812fx.service();
 }
